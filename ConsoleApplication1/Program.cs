e8e9b844d7283756b91f7288a52b8f1fe9b597f4
matdestr <mathisse.de.strooper@gmail.com>
2016-02-20 08:54:01 +0100

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinTestShared;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start testing");
+            MyClass.Auth();
+            var tweetJelle = MyClass.publishTweet();
+
+            //MyClass.Retweet(tweetJelle.Id);
+
+            MyClass.Answer("Hallo Mathisse!" , tweetJelle.Id);
+
+
+
+            
+
+            var tweets = MyClass.getHomeLine();
+            foreach (var tweet in tweets)
+            {
+                Console.WriteLine(tweet.Text);
+                Console.WriteLine(tweet.CreatedAt);
+                Console.WriteLine(tweet.CreatedBy);
+                Console.WriteLine(tweet.Id);
+                Console.WriteLine(tweet.IsTweetPublished);
+            }
+
+            Console.WriteLine("End testing");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ XamarinTestShared/XamarinTestShared/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces;
+using Tweetinvi.Core.Parameters;
+
+namespace XamarinTestShared
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+
+        public static void Auth()
+        {
+            Tweetinvi.Auth.SetUserCredentials("N8U7yLDCRbZqJpo2NstN4bTJT", "VIf5sT9pmpsayeRBUc1MrGRS4zq7qzdDktcMmCLS1aMlGuLZmr", "1065192774-TBevJ5RuGGXUAxpX28EsaNjc6fkjpCOCv4xpo5U", "09q2JmCag9gGeIz29jCjQKTS4NVu6tsI7900V1fXjBiI8");
+
+        }
+
+        public static ITweet publishTweet()
+        {
+            var tweet = Tweet.PublishTweet("Hello Jelle Spoelders!");
+            return tweet;
+        }
+
+        public static IEnumerable<ITweet> getHomeLine()
+        {
+            var user = User.GetLoggedUser();
+            var tweets = Timeline.GetHomeTimeline();
+            return tweets;
+        }
+
+	    public static void Retweet(long id)
+	    {
+	        Tweet.PublishRetweet(id);
+	    }
+
+	    public static void Answer(string text,long id)
+	    {
+	        Tweet.PublishTweetInReplyTo(text, id);
+	    }
+    }
+}
+
